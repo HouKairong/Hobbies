44f517cd6238a555669b360bcfded5393ede7635
HouKairong <houkairong03@gmail.com>
2026-02-23 08:19:01 +0100

    second things second

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,14 +1,171 @@
+import { useState } from "react";
+
 const Books = () => {
+  const [selectedSeries, setSelectedSeries] = useState(null);
+  const [openBooks, setOpenBooks] = useState({});
+
+  const books = {
+    Wiedźmin: [
+      {
+        title: "Ostatnie życzenie",
+        stories: [
+          "Głos rozsądku",
+          "Wiedźmin",
+          "Ziarno prawdy",
+          "Mniejsze zło",
+          "Kwestia ceny",
+          "Kraniec świata",
+          "Ostatnie życzenie",
+        ],
+      },
+      {
+        title: "Miecz przeznaczenia",
+        stories: [
+          "Granica możliwości",
+          "Okruch lodu",
+          "Wieczny ogień",
+          "Trochę poświęcenia",
+          "Miecz przeznaczenia",
+          "Coś więcej",
+        ],
+      },
+      { title: "Krew elfów" },
+      { title: "Czas pogardy" },
+      { title: "Chrzest ognia" },
+      { title: "Wieża Jaskółki" },
+      { title: "Pani Jeziora" },
+      { title: "Sezon Burz" },
+      { title: "Rozdroże Kruków" },
+      { title: "Droga z której się nie wraca" },
+      { title: "Coś się kończy, coś się zaczyna" },
+    ],
+
+    "Pieśń Lodu i Ognia": [
+      { title: "Gra o Tron" },
+      { title: "Starcie Królów" },
+      { title: "Nawałnica Mieczy" },
+      { title: "Uczta dla Wron" },
+      { title: "Taniec ze Smokami" },
+      { title: "Ogień i Krew" },
+      { title: "Rycerz Siedmiu Królestw" },
+      { title: "Świat Lodu i Ognia" },
+      { title: "Panowanie Smoka" },
+      { title: "Księżniczka i Królowa" },
+      { title: "Książę Łotrzyk" },
+      { title: "Synowie Smoka" },
+    ],
+
+    "Sherlock Holmes": [
+      {
+        title: "Przygody Sherlocka Holmesa",
+        stories: [
+          "Skandal w Czechach",
+          "Liga Rudowłosych",
+          "Pięć pestek pomarańczy",
+        ],
+      },
+      {
+        title: "Wspomnienia Sherlocka Holmesa",
+        stories: ["Srebrny Płomień", "Ostatnia zagadka"],
+      },
+      { title: "Studium w szkarłacie" },
+      { title: "Znak czterech" },
+      { title: "Pies Baskerville'ów" },
+      { title: "Dolina trwogi" },
+      { title: "Powrót Sherlocka Holmesa" },
+      { title: "Pożegnalny ukłon" },
+      { title: "Sprawy Sherlocka Holmesa" },
+    ],
+    "Książki o Chinach": [
+      { title: "Chiny bez makijażu" },
+      { title: "Czerwony cesarz: Xi Jinping i jego Nowe Chiny" },
+      { title: "Taoizm: Wprowadzenie" },
+      { title: "Baśnie chińskie" },
+      {
+        title:
+          "Prowadzący umarłych: Opowieści prawdziwe Chiny z perspektywy nizin społecznych",
+      },
+      { title: "Tajwan: Herbatka na beczce prochu" },
+      {
+        title: "Japonia, Chiny i Korea: O ludziach skłóonych na śmierć i życie",
+      },
+      { title: "Chiny: Przewodnik po herosach, smokach i świętych rzekach" },
+      { title: "Chiny: Portret cywilizacji i narodu" },
+      {
+        title:
+          "Cesarzowa wdowa Cixi: Konkubin która stworzyła współczesne Chiny",
+      },
+      { title: "Historia Kościoła w Chinach" },
+      { title: "Daodejing" },
+      { title: "Ocalała z chińskiego gułagu" },
+    ],
+
+    "Boska Komedia": [
+      { title: "Inferno" },
+      { title: "Czyściec" },
+      { title: "Raj" },
+    ],
+    "Mitologia Nordycka": [],
+    "Obrona Sokratesa": [],
+  };
+
+  const toggleSeries = (series) => {
+    setSelectedSeries(selectedSeries === series ? null : series);
+    setOpenBooks({}); // reset otwartych tomów przy zmianie serii
+  };
+
+  const toggleBook = (bookTitle) => {
+    setOpenBooks((prev) => ({
+      ...prev,
+      [bookTitle]: !prev[bookTitle],
+    }));
+  };
+
   return (
-    <ul>
-      <li>Wiedźmin</li>
-      <li>Pieśń Lodu i Ognia</li>
-      <li>Sherlock Holmes</li>
-      <li>Książki o Chinach</li>
-      <li>Boska Komedia</li>
-      <li>Mitologia Nordycka</li>
-      <li>Obrona Sokratesa</li>
-    </ul>
+    <div>
+      {/* Lista serii */}
+      <ul>
+        {Object.keys(books).map((series) => (
+          <li
+            key={series}
+            onClick={() => toggleSeries(series)}
+            style={{ cursor: "pointer", fontWeight: "bold" }}
+          >
+            {series}
+          </li>
+        ))}
+      </ul>
+
+      {/* Lista tomów */}
+      {selectedSeries && (
+        <div style={{ marginTop: "10px" }}>
+          <h3>{selectedSeries}</h3>
+          <ul>
+            {books[selectedSeries].map((book) => (
+              <li key={book.title}>
+                <span
+                  onClick={() => (book.stories ? toggleBook(book.title) : null)}
+                  style={{
+                    cursor: book.stories ? "pointer" : "default",
+                  }}
+                >
+                  {book.title}
+                </span>
+
+                {/* Opowiadania */}
+                {book.stories && openBooks[book.title] && (
+                  <ul style={{ marginLeft: "20px" }}>
+                    {book.stories.map((story) => (
+                      <li key={story}>{story}</li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,123 +2,305 @@ import { useState } from "react";
 
 const Movies = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [openSubgroup, setOpenSubgroup] = useState(null);
+  const [openSeries, setOpenSeries] = useState(null);
 
   const movies = {
     Gry: [
-      "Wiedźmin (2001)",
-      "Wiedźmin: Zmora Wilka (2021)",
-      "Wiedźmin: Syreny z głębin (2025)",
-      "Szczury: Opowieści ze świata Wiedźmina (2025)",
-      "Assassin's Creed (2016)",
-      "DOOM (2005)",
-      "DOOM: Anihilacja (2019)",
-      "Lara Croft: Tomb Raider (2001)",
-      "Lara Croft Tomb Raider: Kolebka życia (2003)",
-      "Tomb Raider (2018)",
-      "LEGO Ninjago Film (2017)",
-      "Harry Potter i Kamień Filozoficzny",
-      "Harry Potter i Komnata Tajemnic",
-      "Harry Potter i Więzień Azkabanu",
-      "Harry Potter i Czara Ognia",
-      "Harry Potter i Zakon Feniksa",
-      "Harry Potter i Książę Półkrwi",
-      "Harry Potter i Insygnia Śmierci część 1",
-      "Harry Potter i Insygnia Śmierci część 2",
-      "Jak Wytresować Smoka",
-      "Jak wytresować Smoka 2",
-      "Jak wytresować Smoka 3",
-      "Jak wytresować Smoka",
-      "Gwiezdne Wojny: Mroczne Widmo",
-      "Gwiezdne Wojny: Atak klonów",
-      "Gwiezdne Wojny: Zemsta Sithów",
-      "Gwiezdne wojny: Nowa Nadzieja",
-      "Gwiezdne Wojny: Imperium kontratakuje",
-      "Gwiezdne Wojny: Powrót Jedi",
-      "Gwiezdne Wojny: Przebudzenie Mocy",
-      "Gwiezdne Wojny: Ostatni Jedi",
-      "Gwiezdne Wojny: Skywalker: Odrodzenie",
-      "Sherlock Holmes",
-      "Sherlock Holmes: Gra cieni",
-      "Persona 3 The Movie #1 Spring of Birth",
-      "Persona 3 The Movie #2 Midsummer Knight's Dream",
-      "Persona 3 The Movie #3 Falling Down",
-      "Persona 3 The Movie #4 Winter of Rebirth",
-      "Epoka Lodowcowa",
-      "Epoka Lodowcowa 2 Odwilż",
-      "Epoka Lodowcowa 3 Era Dinozaurów",
-      "Epoka Lodowcowa 4 Wędrówka Kontynentów",
-      "Epoka Lodowcowa 5 Mocne Uderzenie",
-      "Dante's Inferno",
-      "Madagaskar",
-      "Madagaskar 2",
-      "Madagaskar 3",
-      "Pingwiny z Madagaskaru",
-      "Kung Fu Panda",
-      "Kung Fu Panda 2",
-      "Kung Fu Panda 3",
-      "Kung Fu Panda 4",
-      "Herkules",
-      "Aladyn",
-      "Król Lew",
-      "Super Mario Bros",
+      {
+        title: "Wiedźmin",
+        parts: [
+          "Wiedźmin (2001)",
+          "Wiedźmin: Zmora Wilka (2021)",
+          "Wiedźmin: Syreny z głębin (2025)",
+          "Szczury: Opowieści ze świata Wiedźmina (2025)",
+        ],
+      },
+      { title: "Assassin's Creed (2016)" },
+      {
+        title: "DOOM",
+        parts: ["DOOM (2005)", "DOOM: Anihilacja (2019)"],
+      },
+      {
+        title: "Lara Croft",
+        parts: [
+          "Tomb Raider (2001)",
+          "Kolebka życia (2003)",
+          "Tomb Raider (2018)",
+        ],
+      },
+      { title: "LEGO Ninjago Film (2017)" },
+      {
+        title: "Harry Potter",
+        parts: [
+          "Kamień Filozoficzny",
+          "Komnata Tajemnic",
+          "Więzień Azkabanu",
+          "Czara Ognia",
+          "Zakon Feniksa",
+          "Książę Półkrwi",
+          "Insygnia Śmierci cz.1",
+          "Insygnia Śmierci cz.2",
+        ],
+      },
+      {
+        title: "Jak Wytresować Smoka",
+        parts: ["Część 1", "Część 2", "Część 3"],
+      },
+      {
+        title: "Gwiezdne wojny",
+        parts: [
+          "Mroczne Widmo",
+          "Atak klonów",
+          "Zemsta Sithów",
+          "Nowa Nadzieja",
+          "Imperium kontratakuje",
+          "Powrót Jedi",
+          "Przebudzenie Mocy",
+          "Ostatni Jedi",
+          "Skywalker: Odrodzenie",
+        ],
+      },
+      {
+        title: "Sherlock Holmes",
+        parts: ["Sherlock Holmes", "Gra cieni"],
+      },
+      {
+        title: "Persona 3 The Movie",
+        parts: [
+          "#1 Spring of Birth",
+          "#2 Midsummer Knight's Dream",
+          "#3 Falling Down",
+          "#4 Winter of Rebirth",
+        ],
+      },
+      {
+        title: "Epoka Lodowcowa",
+        parts: [
+          "Część 1",
+          "Odwilż",
+          "Era Dinozaurów",
+          "Wędrówka Kontynentów",
+          "Mocne Uderzenie",
+        ],
+      },
+      { title: "Dante's Inferno: An Animated Epic" },
+      {
+        title: "Madagaskar",
+        parts: ["Część 1", "Część 2", "Część 3", "Pingwiny z Madagaskaru"],
+      },
+      {
+        title: "Kung Fu Panda",
+        parts: ["Część 1", "Część 2", "Część 3", "Część 4"],
+      },
+      { title: "Herkules" },
+      { title: "Aladyn" },
+      { title: "Król Lew" },
+      { title: "Super Mario Bros" },
     ],
+
     Seriale: [
-      "Wiedźmin (2001)",
-      "Wiedźmin: Zmora Wilka (2021)",
-      "Wiedźmin: Syreny z głębin (2025)",
-      "Szczury: Opowieści ze świata Wiedźmina (2025)",
-      "Jak Wytresować Smoka",
-      "Jak wytresować Smoka 2",
-      "Jak wytresować Smoka 3",
-      "Jak wytresować Smoka",
-      "El Camino",
-      "Sherlock Holmes",
-      "Sherlock Holmes: Gra cieni",
-      "Persona 3 The Movie #1 Spring of Birth",
-      "Persona 3 The Movie #2 Midsummer Knight's Dream",
-      "Persona 3 The Movie #3 Falling Down",
-      "Persona 3 The Movie #4 Winter of Rebirth",
-      "Madagaskar",
-      "Madagaskar 2",
-      "Madagaskar 3",
-      "Pingwiny z Madagaskaru",
-      "Kung Fu Panda",
-      "Kung Fu Panda 2",
-      "Kung Fu Panda 3",
-      "Kung Fu Panda 4",
-      "LEGO Ninjago Film (2017)",
-      "Death Note",
-      "Avatar: Ostatni Władca Wiatru",
+      {
+        title: "Wiedźmin",
+        parts: [
+          "Wiedźmin (2001)",
+          "Wiedźmin: Zmora Wilka (2021)",
+          "Wiedźmin: Syreny z głębin (2025)",
+          "Szczury: Opowieści ze świata Wiedźmina (2025)",
+        ],
+      },
+      {
+        title: "Jak Wytresować Smoka",
+        parts: ["Część 1", "Część 2", "Część 3"],
+      },
+      { title: "El Camino" },
+      {
+        title: "Sherlock Holmes",
+        parts: ["Sherlock Holmes", "Gra cieni"],
+      },
+      {
+        title: "Persona 3 The Movie",
+        parts: [
+          "#1 Spring of Birth",
+          "#2 Midsummer Knight's Dream",
+          "#3 Falling Down",
+          "#4 Winter of Rebirth",
+        ],
+      },
+      {
+        title: "Madagaskar",
+        parts: ["Część 1", "Część 2", "Część 3", "Pingwiny z Madagaskaru"],
+      },
+      {
+        title: "Kung Fu Panda",
+        parts: ["Część 1", "Część 2", "Część 3", "Część 4"],
+      },
+      { title: "LEGO Ninjago Film (2017)" },
+      { title: "Death Note" },
+      { title: "Avatar: Ostatni Władca Wiatru" },
+      { title: "Herkules" },
     ],
+
     Chiny: [
-      "Kung Fu Panda",
-      "Kung Fu Panda 2",
-      "Kung Fu Panda 3",
-      "Kung Fu Panda 4",
-      "Ne Zha",
-      "Ne Zha 2",
-      "Małpi Król",
-      "Wielki Mur",
-      "Mulan",
-      "Mulan 2",
-      "Mulan",
-      "Rower z Pekinu",
-      "Spirited Away: W Krainie Bogów",
-      "K-Popowe Łowczynie Demonów",
-      "LEGO Ninjago: Film",
-      "Persona 3 The Movie #1 Spring of Birth",
-      "Persona 3 The Movie #2 Midsummer Knight's Dream",
-      "Persona 3 The Movie #3 Falling Down",
-      "Persona 3 The Movie #4 Winter of Rebirth",
-      "Raya i Ostatni Smok",
-      "Behemoth",
-      "Przyczajony Tygrys, Ukryty Smok",
-      "Przyczajony Tygrys, Ukryty Smok: Miecz Przeznaczenia",
-      "The Yinyang Master",
+      {
+        title: "Kung Fu Panda",
+        parts: ["Część 1", "Część 2", "Część 3", "Część 4"],
+      },
+      {
+        title: "Ne Zha",
+        parts: ["Ne Zha", "Ne Zha 2"],
+      },
+      { title: "Małpi Król" },
+      { title: "Wielki Mur" },
+      {
+        title: "Mulan",
+        parts: ["Mulan", "Mulan 2", "Live Action"],
+      },
+      { title: "Rower z Pekinu" },
+      { title: "Spirited Away: W Krainie Bogów" },
+      { title: "K-Popowe Łowczynie Demonów" },
+      { title: "LEGO Ninjago: Film" },
+      { title: "Raya i Ostatni Smok" },
+      { title: "Behemoth" },
+      {
+        title: "Przyczajony Tygrys, Ukryty Smok",
+        parts: [
+          "Przyczajony Tygrys, Ukryty Smok",
+          "Przyczajony Tygrys, Ukryty Smok: Miecz Przeznaczenia",
+        ],
+      },
+      { title: "The Yinyang Master" },
+      { title: "Konfucjusz" },
     ],
-    BoskaKomedia: [],
+
+    BoskaKomedia: {
+      PiekłoListaA: [
+        { title: "L'Inferno" },
+        { title: "Dante's Inferno: An Animated Epic" },
+        { title: "Behemoth" },
+        { title: "DOOM", parts: ["DOOM", "DOOM: Anihilacja"] },
+        { title: "Jako w piekle, tak i na ziemi" },
+        { title: "Norymberga" },
+        { title: "Czas Apokalipsy" },
+        {
+          title: "Hellraiser",
+          parts: [
+            "Hellraiser: Wysłannik piekieł",
+            "Hellraiser: Wysłannik piekieł II",
+            "Hellraiser III: Piekło na ziemi",
+            "Hellraiser IV: Dziedzictwo krwi",
+            "Hellraiser V: Wrota piekieł",
+            "Hellraiser: Droga do piekła",
+            "Hellraiser: Sekta",
+            "Hellraiser: Hellworld.com",
+            "Hellraiser: Revelations",
+            "Hellraiser: Judgment",
+            "Hellraiser ",
+          ],
+        },
+        { title: "Constantine" },
+      ],
+
+      PiekłoListaB: [
+        { title: "L'Inferno" },
+        { title: "Dante's Inferno: An Animated Epic" },
+        { title: "Behemoth" },
+        { title: "DOOM", parts: ["DOOM", "DOOM: Anihilacja"] },
+        { title: "Jako w piekle, tak i na ziemi" },
+        { title: "Minecraft: Film" },
+        { title: "Trylogia Dana Browna" },
+        {
+          title: "Hellraiser",
+          parts: [
+            "Hellraiser: Wysłannik piekieł",
+            "Hellraiser: Wysłannik piekieł II",
+            "Hellraiser III: Piekło na ziemi",
+            "Hellraiser IV: Dziedzictwo krwi",
+            "Hellraiser V: Wrota piekieł",
+            "Hellraiser: Droga do piekła",
+            "Hellraiser: Sekta",
+            "Hellraiser: Hellworld.com",
+            "Hellraiser: Revelations",
+            "Hellraiser: Judgment",
+            "Hellraiser ",
+          ],
+        },
+        { title: "Constantine" },
+      ],
+
+      Czyściec: [
+        { title: "Wiedźmin" },
+        { title: "Dzwonnik z Notre Dame", parts: ["Część 1", "Część 2"] },
+        { title: "Ostatni Pojedynek" },
+        {
+          title: "Śródziemie",
+          parts: [
+            "Władca Pierścieni: Drużyna Pierścienia",
+            "Władca Pierścieni: Dwie Wieże",
+            "Władca Pierścieni: Powrót Króla",
+            "Hobbit: Niezwykła Podróż",
+            "Hobbit: Pustkowie Smauga",
+            "Hobbit: Bitwa Pięciu Armii",
+            "Władca Pierścieni: Wojna Rohirrimów",
+          ],
+        },
+        { title: "Siódma pieczęć" },
+        { title: "Wiking" },
+        { title: "Czarownica: Bajka ludowa z Nowej Anglii" },
+      ],
+
+      Niebo: [
+        { title: "Między piekłem a niebem" },
+        { title: "Niebo istnieje... naprawdę" },
+        { title: "Wszystkie psy idą do Nieba" },
+        { title: "Herkules" },
+        {
+          title: "Starcie Tytanów",
+          parts: ["Starcie Tytanów", "Zemsta Tytanów"],
+        },
+        { title: "Pasja" },
+        { title: "Noe: Wybrany przez Boga" },
+        {
+          title: "Avatar",
+          parts: ["Avatar", "Avatar: Istota wody", "Avatar: Ogień i popiół"],
+        },
+        { title: "Interstellar" },
+        {
+          title: "Gwiezdne wojny",
+          parts: [
+            "Mroczne Widmo",
+            "Atak klonów",
+            "Zemsta Sithów",
+            "Nowa Nadzieja",
+            "Imperium kontratakuje",
+            "Powrót Jedi",
+            "Przebudzenie Mocy",
+            "Ostatni Jedi",
+            "Skywalker: Odrodzenie",
+          ],
+        },
+      ],
+    },
+
     Filozofia: [],
   };
+
+  const handleCategoryChange = (value) => {
+    setSelectedCategory(value);
+    setOpenSubgroup(null);
+    setOpenSeries(null);
+  };
+
+  const toggleSubgroup = (group) => {
+    setOpenSubgroup(openSubgroup === group ? null : group);
+    setOpenSeries(null);
+  };
+
+  const toggleSeries = (title) => {
+    setOpenSeries(openSeries === title ? null : title);
+  };
+
+  const isBoskaKomedia = selectedCategory === "BoskaKomedia";
 
   return (
     <div>
@@ -126,14 +308,12 @@ const Movies = () => {
         Wybierz licencję:
         <select
           value={selectedCategory}
-          onChange={(e) => setSelectedCategory(e.target.value)}
+          onChange={(e) => handleCategoryChange(e.target.value)}
         >
-          {/* placeholder */}
           <option value="" disabled hidden>
             Wybierz licencję
           </option>
 
-          {/* faktyczne opcje */}
           {Object.keys(movies).map((cat) => (
             <option key={cat} value={cat}>
               {cat}
@@ -142,12 +322,69 @@ const Movies = () => {
         </select>
       </label>
 
-      {selectedCategory && (
+      {selectedCategory && !isBoskaKomedia && (
         <ul>
-          {movies[selectedCategory].map((movie) => (
-            <li key={movie}>{movie}</li>
+          {movies[selectedCategory].map((item) => (
+            <li key={item.title}>
+              <span
+                onClick={() => (item.parts ? toggleSeries(item.title) : null)}
+                style={{
+                  cursor: item.parts ? "pointer" : "default",
+                }}
+              >
+                {item.title}
+              </span>
+
+              {item.parts && openSeries === item.title && (
+                <ul style={{ marginLeft: "20px" }}>
+                  {item.parts.map((part) => (
+                    <li key={part}>{part}</li>
+                  ))}
+                </ul>
+              )}
+            </li>
           ))}
         </ul>
+      )}
+
+      {isBoskaKomedia && (
+        <div>
+          {Object.keys(movies.BoskaKomedia).map((group) => (
+            <div key={group}>
+              <h3
+                onClick={() => toggleSubgroup(group)}
+                style={{ cursor: "pointer" }}
+              >
+                {group}
+              </h3>
+
+              {openSubgroup === group && (
+                <ul>
+                  {movies.BoskaKomedia[group].map((item) => (
+                    <li key={item.title}>
+                      <span
+                        onClick={() =>
+                          item.parts ? toggleSeries(group + item.title) : null
+                        }
+                        style={{ cursor: item.parts ? "pointer" : "default" }}
+                      >
+                        {item.title}
+                      </span>
+
+                      {item.parts && openSeries === group + item.title && (
+                        <ul style={{ marginLeft: "20px" }}>
+                          {item.parts.map((part) => (
+                            <li key={part}>{part}</li>
+                          ))}
+                        </ul>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
